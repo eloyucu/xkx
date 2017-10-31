cc307d3e7cc537b9d083fa4489080b4c764196fa
eloyucu <rockacolla@hotmail.com>
2017-10-31 17:00:29 +0100

    Change name app on mix file.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule XK.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :xk,
+    [app: :xkx,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
